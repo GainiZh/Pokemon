429ec462f40937fa4c94fe0e660c04e3fd2b9d8e
GainiZh <gaini.zhulamanova@gmail.com>
2019-05-27 20:26:15 +0200

    get api

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// configure api request
+let xhr = new XMLHttpRequest();
+// get the API data 
+xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/151", true);
+
+xhr.onload = function() {
+    let pokeData = JSON.parse(xhr.responseText);
+    console.log(pokeData);
+    loaded(pokeData);
+}
+
+xhr.send();
+
+function loaded(pokeData) {
+    // name
+    let name = document.querySelector('.name');
+    name.innerHTML = pokeData.species.name;
+
+    // type
+    let type = document.querySelector('.type');
+    let typeName = pokeData.types['0'].type.name;
+    type.innerHTML = `Type: ${typeName} pokemon`;
+
+    // images
+
+    document.querySelector('.image1').src = pokeData.sprites.front_default;
+    document.querySelector('.image2').src = pokeData.sprites.back_default;
+    document.querySelector('.image3').src = pokeData.sprites.front_shiny;
+    document.querySelector('.image4').src = pokeData.sprites.back_shiny;
+}
+
